67400b748e6553f7a06d4edfce1faab5992435d3
Zack Corr <zack@z0w0.me>
2013-07-26 15:56:47 +1000

    Add keyboard variant conversion tests in a similar fashion to the mouse tests.. kinda ugly

--- tests/Main.hs
+++ tests/Main.hs
@@ -2,6 +2,10 @@ module Main where
 
 import Test.Framework (defaultMain, testGroup)
 import qualified Color
+import qualified Mouse
+import qualified Keyboard
 
 main :: IO ()
-main = defaultMain [testGroup "Color" Color.tests]
+main = defaultMain [testGroup "Color" Color.tests,
+                    testGroup "Keyboard" Keyboard.tests,
+                    testGroup "Mouse" Mouse.tests]
--- tests/Mouse.hs
+++ tests/Mouse.hs
@@ -10,6 +10,6 @@ import qualified Graphics.UI.SDL.Utilities as Util
 {- Helm's mouse variants map directly to SDL's. If that doesn't work, then it will break completely.
    So we check it here. -}
 tests :: [Test]
-tests = [testCase "left mouse variant <=> SDL's variant" (Util.fromEnum SDL.ButtonLeft @=? fromIntegral (fromEnum LeftMouse)),
-         testCase "middle mouse variant <=> SDL's variant" (Util.fromEnum SDL.ButtonMiddle @=? fromIntegral (fromEnum MiddleMouse)),
-         testCase "right mouse variant <=> SDL's variant" (Util.fromEnum SDL.ButtonRight @=? fromIntegral (fromEnum RightMouse))]
+tests = [testCase "left mouse <=> SDL" (Util.fromEnum SDL.ButtonLeft @=? fromIntegral (fromEnum LeftMouse)),
+         testCase "middle mouse <=> SDL" (Util.fromEnum SDL.ButtonMiddle @=? fromIntegral (fromEnum MiddleMouse)),
+         testCase "right mouse <=> SDL" (Util.fromEnum SDL.ButtonRight @=? fromIntegral (fromEnum RightMouse))]
