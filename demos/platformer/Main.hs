ff0242998e2490a9a3fcb0781eab9e41e72e0621
Zack Corr <zack@z0w0.me>
2013-07-31 19:20:39 +1000

    remove old platformer.hs file and cleanup platformer demo

--- demos/platformer/Main.hs
+++ demos/platformer/Main.hs
@@ -7,13 +7,10 @@ import qualified FRP.Helm.Keyboard as Keyboard
 
 data Direction = Left | Right deriving Show
 
-data Mario =
-  Mario { mX :: Double, mY :: Double,
-          vX :: Double, vY :: Double,
-          dir :: Direction } deriving Show
+data Mario = Mario Double Double Double Double Direction deriving Show
 
 jump :: (Int, Int) -> Mario -> Mario
-jump (_, y) m@(Mario mX mY vX vY dir)
+jump (_, y) m@(Mario mX mY vX _ dir)
   | y < 0 && mY == 0 = Mario mX mY vX 5 dir
   | otherwise = m 
 
@@ -30,21 +27,21 @@ walk :: (Int, Int) -> Mario -> Mario
 walk (x, _) (Mario mX mY _ vY dir) =
   Mario mX mY (fromIntegral x) vY (whatDir x)
   where
-    whatDir x
-      | x < 0 = Left
-      | x > 0 = Right
+    whatDir dx
+      | dx < 0 = Left
+      | dx > 0 = Right
       | otherwise = dir
 
 step :: (Time, (Int, Int)) -> Mario -> Mario
 step (t, arrows) = physics t . walk arrows . gravity t . jump arrows
 
 render :: (Int, Int) -> Mario -> Element
-render (w, h) (Mario mX1 mY1 vX1 vY1 dir1) =
+render (w, h) (Mario mX1 mY1 _ _ _) =
   collage w h [move (half w, half h) $ filled blue $ rect (fromIntegral w) (fromIntegral h), 
                move (half w, fromIntegral h - half 50) $ filled green $ rect (fromIntegral w) 50, 
                move (mX1, mY1 + fromIntegral h - 62) $ filled red $ rect 24 28] 
   where
-    half n = fromIntegral $ n `div` 2
+    half = (/ 2). fromIntegral
 
 input :: SignalGen (Signal (Time, (Int, Int)))
 input = lift2 (,) delta' Keyboard.arrows
