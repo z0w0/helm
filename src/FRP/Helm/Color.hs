e2110a968d97f227850976dbf27679448f1bfa6b
Zack Corr <zack@z0w0.me>
2013-11-12 18:33:04 +1000

    Add guards for making sure components passed to RGB[A] colors are between 0 and 1

--- src/FRP/Helm/Color.hs
+++ src/FRP/Helm/Color.hs
@@ -42,11 +42,16 @@ data Color = Color Double Double Double Double deriving (Show, Eq, Ord, Read, Ge
 
 {-| Creates an RGB color. -}
 rgb :: Double -> Double -> Double -> Color
-rgb r g b = Color r g b 1
+rgb r g b = rgba r g b 1
 
 {-| Creates an RGB color, with transparency. -}
 rgba :: Double -> Double -> Double -> Double -> Color
-rgba = Color
+rgba r g b a
+  | r < 0 || r > 1 ||
+    g < 0 || g > 1 ||
+    b < 0 || b > 1 ||
+    a < 0 || a > 1 = error "FRP.Helm.Color.rgba: color components must be between 0 and 1"
+  | otherwise = Color r g b a
 
 {-| A bright red color. -}
 red :: Color
